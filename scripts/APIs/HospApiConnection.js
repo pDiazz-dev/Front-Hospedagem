36ac9442a105c9886283daa1e2ed1e074f651c21
Matheus <matheusguilherme1520@gmail.c>
2025-10-17 18:00:12 -0300

    Inicio do tratamento de erros.

--- scripts/APIs/HospApiConnection.js
+++ scripts/APIs/HospApiConnection.js
@@ -1,16 +1,53 @@
+function formatarNomeCompleto(nome) {
+  const excecoes = ['de', 'da', 'do', 'dos', 'das', 'e'];
+  
+  return nome
+    .toLowerCase()
+    .split(' ')
+    .map(palavra => 
+      excecoes.includes(palavra) ? palavra : palavra.charAt(0).toUpperCase() + palavra.slice(1)
+    )
+    .join(' ');
+}
+
+
 const CadNewHospede = document.getElementById('saveBtn');
 CadNewHospede.addEventListener("click", () =>{
     let nome = document.getElementById('nomeHospede').value.trim();
-    let telefone = document.getElementById('telefoneHospede').value;
-    let cpf = document.getElementById('cpfHospede').value;
+    let telefone = document.getElementById('telefoneHospede').value.replace(/\D/g, '');
+    let cpf = document.getElementById('cpfHospede').value.replace(/\D/g, '');
+
+    if(telefone.toString().length != 11 || isNaN(telefone)){
+        document.querySelector('.personalNumbQuantErr').style.display = 'block';
+        return;
+    }
+    
+
+    if(cpf.toString().length != 11 || isNaN(cpf)){
+        document.querySelector('.personalCpfQuantErr').style.display = 'block';
+        return;
+    }
+
+    nome = formatarNomeCompleto(nome);
+
 
     fetch(`${config.API_URL}/new-guest/sigin-guest`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
-        body:  JSON.stringify({
-            nome, telefone, cpf
-        })
+        body: JSON.stringify({ nome, telefone, cpf })
     })
-})
+    .then(response =>
+        response.json().then(data => ({
+            status: response.status,
+            body: data
+        }))
+    )
+    .then(({ status, body }) => {
+        if (status === 500) {
+            alert("Hóspede já cadastrado!");
+        
+    }})
+});
+
