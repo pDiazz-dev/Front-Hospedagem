7ff438552073413879dc9de061692f8ebcbe1cc9
Theus21238 <matheusguilherme1520@gmail.com>
2025-11-13 16:28:22 -0300

    feat(RoomDialog, ListarQuartos): Adicionando dialogs para criar um hóspede
    Conexão com o banco de dados com método GET e POST

---
+++ scripts/APIs/ListarQuartos.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  listarQuartos();
+});
+
+let quartos = [];
+
+async function listarQuartos() {
+  const tabelaBody = document.querySelector('.rooms-table-Body');
+  if (!tabelaBody) {
+    console.error("Elemento '.rooms-table-Body' não encontrado!");
+    return;
+  }
+
+  tabelaBody.innerHTML = '';
+
+  try {
+    const response = await fetch(`${config.API_URL}/quartos`);
+    if (!response.ok) {
+      console.error('Erro ao buscar quartos');
+      return;
+    }
+
+    quartos = await response.json();
+
+    quartos.forEach(quarto => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${quarto.tipoQuarto}</td>
+        <td>${quarto.numeroQuarto}</td>
+        <td>R$ ${Number(quarto.valorQuarto).toFixed(2)}</td>
+        <td>${quarto.ativo ? 'Ativo' : 'Inativo'}</td>
+      `;
+      tabelaBody.appendChild(row);
+    });
+
+  } catch (error) {
+    console.error('Erro ao processar a lista de quartos:', error);
+  }
+}
